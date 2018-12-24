3722956502f31ead4ce8c4bf96016379162b4a92
D-SF41EV <Mohamed-Larbi.Chouaiar@ing.com>
2018-12-24 18:58:03 +0100

    Add polymer folder

--- angular/angular-with-vue/src/app/app.component.ts
+++ angular/angular-with-vue/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import '../assets/my-custom-element';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-with-vue';
+  message: string = "message from Angular world";
+  count: number = 0;
+
+  updateCounter(event) {
+    this.count = event.detail[0]
+  }
 }
--- angular/angular-with-vue/src/app/app.module.ts
+++ angular/angular-with-vue/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonModule, MatInputModule} from "@angular/material";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
